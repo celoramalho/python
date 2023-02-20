b4b1940d740baa8fd97c2c94785cc9ebfa3f9266
Marcelo Ramalho <marceloramalho.info@gmail.com>
2023-02-19 21:32:27 -0300

    Add files via upload

--- python_joguinhos/forca.py
+++ python_joguinhos/forca.py
@@ -29,11 +29,29 @@ def imprime_mensagem_abertura():
 
 
 def imprime_mensagem_vencedor():
-    print("Você Ganhou!")
+    os.system('cls') or None
+    print("      VOCÊ ACERTOU      ")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
 
 
 def imprime_mensagem_perdedor():
-    print("Você Perdeu")
+    os.system('cls') or None
+    print(" _______\n|/      |          =======                                             ||         ||")
+    print("|       O        ||       ||  _______       __           ||   ______   ||         ||")
+    print("|      /|\       ||       || ||     || || //   \   ======|| ||      || ||         ||")
+    print("|      / \       ||=======   ||======  ||//      ||      || ||======   ||         ||")
+    print("|                ||          ||        ||        ||      || ||         ||         ||")
+    print("---              ||            ======  ||          ======||   ======     =========\n")
 
 
 def forca_desenho(erros):
@@ -87,11 +105,21 @@ def carrega_palavra_secreta():
     return palavra_secreta
 
 
+def jogar_novamente():
+    jogar_denovo = input("Jogar Novamete? (S | N): ")
+
+    if (jogar_denovo.upper() == "S"):
+        jogar()
+    elif (jogar_denovo.upper() == "N"):
+        print("Obrigado por jopgar! XD")
+
+
 def jogar():
 
     imprime_mensagem_abertura()
     palavra_secreta = carrega_palavra_secreta()
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    letras_chutadas = []
 
     print(letras_acertadas)
 
@@ -106,17 +134,23 @@ def jogar():
             marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros += 1      # erros = erros + 1
+            letras_chutadas.append(chute)
             print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
 
         enforcou = erros == 6       # if (erros == 6): enforcou = true
         forca_desenho(erros)
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+        print("Letras erradas: ", letras_chutadas)
 
     if (acertou):
         imprime_mensagem_vencedor()
+        print("A palavra era ", palavra_secreta)
     else:
         imprime_mensagem_perdedor()
+        print("A palavra era ", palavra_secreta)
+
+    jogar_novamente()
 
 
 if (__name__ == "__main__"):
