db2dca54e60b0c5e4ca28796b84151758379a844
Marcelo Ramalho <marceloramalho.info@gmail.com>
2023-02-21 16:41:55 -0300

    Add files via upload

--- python_joguinhos/jogos.py
+++ python_joguinhos/jogos.py
@@ -1,5 +1,6 @@
 import adivinhacao
 import forca
+import jogodavelha
 
 
 def escolhe_jogo():
@@ -7,16 +8,21 @@ def escolhe_jogo():
     print("*******Escolha o seu jogo!*******")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação (3) Jogo da Velha")
 
     jogo = int(input("Qual jogo? "))
 
-    if(jogo == 1):
+    if (jogo == 1):
         print("Jogando forca")
         forca.jogar()
-    elif(jogo == 2):
+    elif (jogo == 2):
         print("Jogando adivinhação")
         adivinhacao.jogar()
+    elif (jogo == 3):
+        print("Jogando Jogo da Velha")
+        jogodavelha.jogar()
+    
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     escolhe_jogo()
